f5c86138d1e80d0c85b5a620bbbd615186f75810
Gregory Goodwin <gregorygoodwin@Gregorys-Mini.lan>
2016-05-18 12:06:36 -0400

    going live

--- common/models/client-token.js
+++ common/models/client-token.js
@@ -2,10 +2,10 @@ var braintree = require('braintree');
 
 module.exports = function(ClientToken) {
 	var gateway = braintree.connect({
-		environment: braintree.Environment.Sandbox,
-		merchantId: "jjt86fgtc386pgqw",
-		publicKey: "7zqtmfxrwmd7p7p7",
-		privateKey: "4bcf950726dff60167e3bf30c5fb1383"
+		environment: braintree.Environment.Production,
+		merchantId: "3td6z59z6r7zr467",
+		publicKey: "xnbc8qt8dcknwkng",
+		privateKey: "76adb8eda0d17244eae3d018c2453546"
 	});
 
 
